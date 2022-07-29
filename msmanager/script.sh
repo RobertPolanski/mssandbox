942eb79c40d3b58a6209d9a0377c6e502fae5f81
Robert Polanski <robert.polanski@instersoft.de>
2022-07-29 19:27:34 +0200

    primitive kubernetes with env

--- msmanager/script.sh
+++ msmanager/script.sh
@@ -3,3 +3,9 @@ docker build -t robertpolanski/msmanager:v1 .
 docker run -p 8080:8080 robertpolanski/msmanager:v1
 
 docker push robertpolanski/msmanager:v1
+
+kubectl delete -f msmanager-service.yml
+kubectl delete -f msmanager-deployment.yml
+
+kubectl create -f msmanager-service.yml
+kubectl create -f msmanager-deployment.yml
