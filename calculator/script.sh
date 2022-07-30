e54e195b4a65f48e611afb6502a0fa50e5f13aa0
Robert Polanski <robert.polanski@instersoft.de>
2022-07-30 16:25:55 +0200

    bug correction

--- calculator/script.sh
+++ calculator/script.sh
@@ -3,3 +3,5 @@ docker build -t robertpolanski/calculator:v1 .
 docker run -p 8080:8080 robertpolanski/calculator:v1
 
 docker push robertpolanski/calculator:v1
+
+kubectl config set-context --current --namespace=app
